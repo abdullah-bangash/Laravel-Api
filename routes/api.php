707e03abec7502ef0eefa5dbb80fe7eed3a45848
abdullah-bangash <abdultech90@gmail.com>
2019-11-14 23:22:49 +0500

    Authentication with JWT

--- routes/api.php
+++ routes/api.php
@@ -12,3 +12,20 @@ use Illuminate\Http\Request;
 Route::group(['prefix' => 'v1'], function () {
     Route::apiResource('/person','Api\v1\PersonController');
 });
+
+
+// Authentication Routes
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('register', 'AuthController@register');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
